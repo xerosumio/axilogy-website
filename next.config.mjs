05c94fbffa8005bca6a67d7194bff54d8b6d4080
hong tse <player3755@gmail.com>
2025-04-23 01:15:15 +0000

    revert, failed

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-  output:'export'
+  // output:'export'
 }
 
 mergeConfig(nextConfig, userConfig)
